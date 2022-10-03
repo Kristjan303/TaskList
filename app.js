59e79fe15f5bdc68d49352bc1eac08d8fafcf744
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-10-03 12:36:32 +0300

    As a user I can remove multiple tasks  [delivers #183413875]

--- app.js
+++ app.js
@@ -1,52 +1,47 @@
-const taskInput = document.querySelector(`#task`)
-const form = document.querySelector(`form`)
-const taskList = document.querySelector(`ul`)
-const  removeall = document.querySelector(`remove-all`)
-form.addEventListener(`submit`, addTask)
-taskList.addEventListener(`click`, deltask)
-removeall.addEventListener(`click`, remall)
+const form = document.querySelector('form')
+const taskinput = document.querySelector('#task')
+const tasklist = document.querySelector('ul')
+const removelist = document.querySelector('#Remove-all')
 
+form.addEventListener("submit", addTask)
+tasklist.addEventListener('click', deltask)
+removelist.addEventListener('click', RemoveAll)
 
+function addTask(e){
+    //create list item
+    console.log(taskinput.value)
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(taskinput.value))
+    li.className = 'collection-item'
+    const  a = document.createElement('a')
+    a.appendChild(document.createTextNode('x'))
+    a.className = 'blue-text text-darken aligin right'
+    a.setAttribute('href', '#')
+    li.appendChild(a)
 
-function  deltask(e){
-    if (e.target.textContent === `X`){
-        if (confirm(`Are you sure you want to delete this task?`)){
-            e.target.parentElement.remove()
-        }
-    }
+    //add to list
+    const ul = document.querySelector('ul')
+    ul.appendChild(li)
+    taskinput.value = ''
+    //remove from list
 
+    e.preventDefault()
 }
 
-function  remall(e){
-    if (e.target.textContent === `Remove all tasks`){
-        if (confirm(`Are you sure?`)){
-            while (taskList.firstChild){
-                taskList.removeChild(taskList.firstChild)
+
+function deltask(e) {
+    if(e.target.textContent === 'x')
+        if (confirm('Are you sure you want do delete this task?')) {
+            e.target.parentElement.remove()
+        }
+}
+
+function RemoveAll(e) {
+    if (e.target.textContent === 'Remove all') {
+        if (confirm('Are you sure you wanna remove all?')){
+            while (tasklist.firstChild){
+                tasklist.removeChild(tasklist.firstChild)
             }
         }
     }
-}
-
-
-
-
-function addTask(e){
-    console.log(taskInput.value)
-    const li = document.createElement(`li`)
-    li.appendChild(document.createTextNode(taskInput.value))
-    li.className = "collection-item"
-    const a = document.createElement(`a`)
-    a.appendChild(document.createTextNode(`X`))
-    a.className = `blue-text text-darken-2 secondary-content`
-    a.setAttribute(`href`, `#`)
-    li.append(`a`)
-
-    const ul = document.querySelector(`ul`)
-    ul.appendChild(li)
-    taskInput.value = ``
-    console.log(li)
-
-
-
-    e.preventDefault()
 }
