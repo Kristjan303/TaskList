2924728b2ff20d770eab6f559203287e00f95ccd
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-10-06 14:28:35 +0300

    As a user I can remove multiple tasks  [delivers #183413875]

--- app.js
+++ app.js
@@ -1,47 +1,105 @@
-const form = document.querySelector('form')
-const taskinput = document.querySelector('#task')
-const tasklist = document.querySelector('ul')
-const removelist = document.querySelector('#Remove-all')
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+const taskList = document.querySelector('ul')
+const delAllTasks = document.querySelector('#del-tasks')
 
-form.addEventListener("submit", addTask)
-tasklist.addEventListener('click', deltask)
-removelist.addEventListener('click', RemoveAll)
+form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
+delAllTasks.addEventListener('click', deleteAllTasks)
+document.addEventListener('DOMContentLoaded', getTasks)
 
 function addTask(e){
-    //create list item
-    console.log(taskinput.value)
+    // create list item
     const li = document.createElement('li')
-    li.appendChild(document.createTextNode(taskinput.value))
+    li.appendChild(document.createTextNode(taskInput.value))
     li.className = 'collection-item'
-    const  a = document.createElement('a')
-    a.appendChild(document.createTextNode('x'))
-    a.className = 'blue-text text-darken aligin right'
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2 secondary-content'
     a.setAttribute('href', '#')
     li.appendChild(a)
-
-    //add to list
+    // add to list
     const ul = document.querySelector('ul')
     ul.appendChild(li)
-    taskinput.value = ''
-    //remove from list
-
+    addTaskLS(taskInput.value)
+    taskInput.value = ''
     e.preventDefault()
 }
 
-
-function deltask(e) {
-    if(e.target.textContent === 'x')
-        if (confirm('Are you sure you want do delete this task?')) {
+function deleteTask(e){
+    if(e.target.textContent == 'X'){
+        if(confirm('Are you sure to delete this task?')){
             e.target.parentElement.remove()
+            deleteTaskLS(e.target.parentElement.textContent.slice(0,-1))
         }
+    }
 }
 
-function RemoveAll(e) {
-    if (e.target.textContent === 'Remove all') {
-        if (confirm('Are you sure you wanna remove all?')){
-            while (tasklist.firstChild){
-                tasklist.removeChild(tasklist.firstChild)
-            }
+function deleteAllTasks(e){
+    //taskList.innerHTML = ''
+    while (taskList.firstChild){
+        taskList.removeChild(taskList.firstChild)
+    }
+    localStorage.removeItem('tasks')
+}
+
+function addTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function deleteTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((taskLS, taskIndex) => {
+        if(taskLS === task){
+            tasks.splice(taskIndex, 1)
+        }
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function getTasks(){
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((task) => {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(task))
+        li.className = 'collection-item'
+        const a = document.createElement('a')
+        a.appendChild(document.createTextNode('X'))
+        a.className = 'blue-text text-darken-2 secondary-content'
+        a.setAttribute('href', '#')
+        li.appendChild(a)
+        // add to list
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
+    })
+}
+
+function filterTasks() {
+    let tasks = document.querySelectorAll('li')
+    let search_query = document.getElementById("myInput").value;
+    for (var i = 0; i < tasks.length; i++) {
+        if(tasks[i].innerText.slice(0, -1).toLowerCase()
+            .includes(search_query.toLowerCase())) {
+            tasks[i].hidden = false
+        } else {
+            tasks[i].hidden = true
         }
     }
 }
